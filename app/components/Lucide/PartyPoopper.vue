02cc4551de4f6106503f40a71805cbc52ca4cd40
Deounsamnang <Deounsamnang7@gmail.com>
2025-04-01 15:33:38 +0700

    Class Prop for Style in PartyPoopper.

--- app/components/Lucide/PartyPoopper.vue
+++ app/components/Lucide/PartyPoopper.vue
@@ -1,6 +1,7 @@
 <template>
     <div :class="[
         'cursor-pointer select-none p-2 hover:bg-accent rounded-md transition-colors duration-200 flex items-center justify-center',
+        props.class
     ]" @mouseenter="handleMouseEnter" @mouseleave="handleMouseLeave">
         <svg xmlns="http://www.w3.org/2000/svg" :width="size" :height="size" viewBox="0 0 24 24" fill="none"
             stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
